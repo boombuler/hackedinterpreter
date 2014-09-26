ae1cb1c3b05b1e9c9796ee1853d132d3afe1aa78
boombuler <boombuler@googlemail.com>
2014-09-26 20:17:27 +0200

    more tests

--- lists_test.go
+++ lists_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 )
 
-func Test_GetIndex(t *testing.T) {
+func Test_Lists(t *testing.T) {
 	tests := map[string]Value{
-		"[0, 1, 2].length":                  3,
-		"[0, 1, 2][1]":                      1,
-		"var_a = [1]; var_a[0] = 2; var[0]": 2,
+		"new_list(1).map(function var_a -> var_a + 2)[0]": 2,
+		"[0, 1, 2].length":                                3,
+		"[0, 1, 2][1]":                                    1,
+		"var_a = [1]; var_a[0] = 2; var[0]":               2,
+		"[[1, 2, 3]].push(1).length":                      2,
 	}
 	for code, val := range tests {
 		value, err := ExecuteString(code, DefaultTimeout)
