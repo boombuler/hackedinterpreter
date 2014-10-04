407ae655abae3c15ff9e9ff27babbef152fc65f0
boombuler <boombuler@googlemail.com>
2014-10-04 11:31:34 +0200

    added basic break / step / continue logic to debugger

--- runtime/debugger.go
+++ runtime/debugger.go
@@ -1,8 +1,21 @@
 package runtime
 
 import (
+	"../token"
 	"errors"
 )
+
+type DebuggerExecutionMode byte
+
+const (
+	Continue DebuggerExecutionMode = iota
+	Step
+)
+
+type BreakEvent struct {
+	Continue chan<- DebuggerExecutionMode
+	Token    *token.Token
+}
 
 type executor func(c *Callable, ctx *Context) (Value, error)
 
@@ -15,23 +28,47 @@ type Debugger struct {
 	curCtx      *Context
 	mainCtx     *Context
 	curCallable *Callable
+
+	mode DebuggerExecutionMode
+
+	breakEventChan chan<- BreakEvent
+	codeBPs        map[*token.Token]bool // change bool to something else if conditional BPs should be impl.
 }
 
-func AttachDebugger(c *Context) (*Debugger, error) {
+func AttachDebugger(c *Context, be chan<- BreakEvent) (*Debugger, error) {
 	if c == nil || c.parentContext != nil {
 		return nil, errors.New("Debugger can only be attached to main context")
 	}
 	d := &Debugger{
-		mainCtx: c,
+		mainCtx:        c,
+		breakEventChan: be,
+		mode:           Step,
 	}
 	c.executor = d.exec
 	return d, nil
+}
+
+func (d *Debugger) ToggleCodeBreakPoint(t *token.Token) {
+	_, ok := d.codeBPs[t]
+	if ok {
+		delete(d.codeBPs, t)
+	} else {
+		d.codeBPs[t] = true
+	}
+
 }
 
 func (d *Debugger) exec(c *Callable, ctx *Context) (Value, error) {
 	d.curCtx = ctx
 	d.curCallable = c
 	// Check here for BreakPoint etc
-
+	if d.mode == Step || d.codeBPs[c.Token] {
+		// We reached a BP...
+		if d.breakEventChan != nil {
+			res := make(chan DebuggerExecutionMode)
+			d.breakEventChan <- BreakEvent{res, c.Token}
+			d.mode = <-res
+		}
+	}
 	return c.fn(ctx)
 }
